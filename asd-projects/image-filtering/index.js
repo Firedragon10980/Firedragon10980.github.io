5ffaaead5525069256e03aac05e5c6b11e4f0163
Firedragon10980 <138500377+Firedragon10980@users.noreply.github.com>
2026-04-15 16:43:06 +0000

    saving sorting exercise

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -14,26 +14,35 @@ $(document).ready(function () {
 function resetAndRender() {
   reset();
   render($("#display"), image);
+  
 }
 
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
-
+  // try to make outer loop loop through each row in image and inner loop go through each pixel in that row
+applyFilter();
+  for (let i = 0; i < image.length; i++) {
+    for (let j = 0; j < image[i].length; j++) {
+      console.log(image[i][j]);
   // do not change the below line of code
   render($("#display"), image);
 }
-
+  }
+  }
 /////////////////////////////////////////////////////////
 // "apply" and "filter" functions should go below here //
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
-
-
+function applyFilter(){
+var pixel = image[i][j];
+var pixelArray = [rgbStringToArray(pixel)];
+var updatedPixel = rgbArrayToString(pixelArray);
+image[i][j] = updatedPixel;
+}
+// This is where I’ll modify the color values later
 // TODO 9 Create the applyFilterNoBackground function
 
 
